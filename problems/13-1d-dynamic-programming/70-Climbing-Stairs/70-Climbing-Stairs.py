597e965416c874ad88c317f202c347c1a503a24c
Snehashish Reddy Manda <srmanda.cs@gmail.com>
2026-09-07 14:40:14 -0400

    chore: rebalance schedule — shift 70 Climbing Stairs to 2026-09-16

--- problems/13-1d-dynamic-programming/70-Climbing-Stairs/70-Climbing-Stairs.py
+++ problems/13-1d-dynamic-programming/70-Climbing-Stairs/70-Climbing-Stairs.py
@@ -3,7 +3,7 @@ https://leetcode.com/problems/climbing-stairs/
 '''
 
 last_solved     = "2026-07-16"
-revisit_in_days = 56
+revisit_in_days = 62
 difficulty      = "easy"
 topic_tags      = ["dynamic-programming", "recursion", "memoization"]
 times_reviewed  = 6
