0a78e5d9b7a455b84b721d6b91088d39fc5d1a29
Snehashish Reddy Manda <srmanda.cs@gmail.com>
2026-09-08 18:16:30 -0400

    solve 261. Graph Valid Tree [g]

--- problems/11-graphs/261-Graph-Valid-Tree/261-Graph-Valid-Tree.py
+++ problems/11-graphs/261-Graph-Valid-Tree/261-Graph-Valid-Tree.py
@@ -4,7 +4,7 @@ https://leetcode.com/problems/graph-valid-tree/
 
 last_solved     = "2026-09-08"
 revisit_in_days = 1
-times_reviewed  = 0
+times_reviewed  = 1
 difficulty      = "medium"
 topic_tags      = ["depth-first-search", "breadth-first-search", "union-find", "graph"]
 
