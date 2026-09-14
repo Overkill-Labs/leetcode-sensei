b85ff05afd05875a44df53c21735d0c292be62fc
Snehashish Reddy Manda <srmanda.cs@gmail.com>
2026-09-14 18:20:10 -0400

    solve(416): first solve Partition Equal Subset Sum

--- problems/13-1d-dynamic-programming/416-Partition-Equal-Subset-Sum/416-Partition-Equal-Subset-Sum.py
+++ problems/13-1d-dynamic-programming/416-Partition-Equal-Subset-Sum/416-Partition-Equal-Subset-Sum.py
@@ -9,6 +9,21 @@ difficulty      = "medium"
 topic_tags      = ["array", "dynamic-programming", "knapsack-problem", "0-1-knapsack"]
 
 class Solution:
-    def solve(self) -> None:
-        # TODO: implement
-        pass
+    def canPartition(self, nums: List[int]) -> bool:
+        self.S = sum(nums)
+
+        if self.S % 2 != 0:
+            return False
+        
+        from functools import lru_cache
+        
+        @lru_cache(maxsize=None)
+        def backtrack(idx, curr):
+            if idx == len(nums):
+                if curr == (self.S // 2):
+                    return True
+                return False
+            
+            return backtrack(idx + 1, curr + nums[idx]) or backtrack(idx + 1, curr)
+        
+        return backtrack(0, 0)
